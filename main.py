6aa486887d24757838d92b4b5433af226e5c4d95
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-09-19 19:30:06 +0100

    feat: get markdown content

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import sys
 
+def get_markdown(path):
+    with open(path) as f:
+        markdown = f.read()
+        return markdown
+
 def main():
     # check if path is passed
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <path_to_markdown>")
         sys.exit(1)
 
-    print("hello from main")
+    path = sys.argv[1]
+    markdown = get_markdown(path)
+    print(markdown)
 
 if __name__ == "__main__":
     main()
---
+++ markdown/hello.md
@@ -0,0 +1,3 @@
+# Hello world!
+
+this is a hello world
