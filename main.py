9e9bda7d1f1283124db80c1d92450dc9c220036a
Mohammed Ouzaa <ouzaa.contact@gmail.com>
2025-09-27 02:57:07 +0100

    feat: add list words count

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def get_markdown(path):
         sys.exit(0)
 
 
-def isHeading(block):
+def is_heading(block):
     if (
         block.startswith("# ")
         or block.startswith("## ")
@@ -28,10 +28,51 @@ def count_heading_words(markdown):
     count = 0
     blocks = markdown.split("\n\n")
     for block in blocks:
-        if isHeading(block):
+        if is_heading(block):
             # remove any leading # and leading/trailing whitespaces
             stripped_block = block.lstrip("#").strip()
             count += len(stripped_block.split())
+    return count
+
+
+def is_unordered_list(block):
+    lines = block.split("\n")
+    for line in lines:
+        if not line:
+            continue
+        if not line.startswith("- "):
+            return False
+    return True
+
+
+def is_ordered_list(block):
+    lines = block.split("\n")
+    i = 1
+    for line in lines:
+        if not line:
+            continue
+        if not line.startswith(f"{i}. "):
+            return False
+        i += 1
+    return True
+
+
+def count_list_words(markdown):
+    count = 0
+    blocks = markdown.split("\n\n")
+    for block in blocks:
+        if is_unordered_list(block):
+            # remove any leading - and leading/trailing whitespaces
+            for line in block.split("\n"):
+                stripped_line = line.lstrip("-").strip()
+                count += len(stripped_line.split())
+        if is_ordered_list(block):
+            # remove any leading 1. and leading/trailing whitespaces
+            i = 1
+            for line in block.split("\n"):
+                stripped_line = line.lstrip(f"{i}.").strip()
+                count += len(stripped_line.split())
+                i += 1
     return count
 
 
@@ -46,6 +87,9 @@ def main():
 
     heading_words_count = count_heading_words(markdown)
     print(f"heading words count: {heading_words_count}")
+
+    list_words_count = count_list_words(markdown)
+    print(f"list words count: {list_words_count}")
 
 
 if __name__ == "__main__":
--- markdown/hello.md
+++ markdown/hello.md
@@ -3,3 +3,10 @@
 ### this a 3 level heading
 
 this is a hello world
+
+- Word count
+- Ignore code blocks
+- Track links like
+
+1. this is an
+2. ordered list
